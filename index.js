f7c64fe4c9df9b159468c196081d5fdd159e45e2
cediegreyhat <63190840+cediegreyhat@users.noreply.github.com>
2023-03-04 23:22:47 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+  const express = require("express");
+    const app = express();
+    let fetch = require("node-fetch");
+    var bodyParser = require("body-parser");
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.get("/webhook", (req, res) => {
+      // the webhook GET route checks the query of the webhook route
+      // to see if the hub.verify_token parameter is equal to the same
+      // callback token set on the facebook app dashboard
+      // this is a security check
+      //remember that I set "devc" as the callback parameter on 
+      // the facebook app dashboard
+      if (req.query["hub.verify_token"] === "devc") {
+        res.send(req.query["hub.challenge"]);
+      } else {
+        res.send("Error, wrong validation token");
+      }
+    });
+    // listen for requests 
+    const listener = app.listen(process.env.PORT, () => {
+      console.log("Your app is listening on port " + listener.address().port);
+    });
